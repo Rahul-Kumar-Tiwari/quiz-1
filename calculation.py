beefdf3550476ce0313c6edff131f2738b809ffb
malay190 <malaykumar089@gmail.com>
2018-06-24 10:52:54 +0530

    calculation

---
+++ calculation.py
@@ -0,0 +1,96 @@
+#calculation
+
+print("_____welcome_____")
+print("choose numbers below to perform various operation")
+print("")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Raise to power")
+print("5. Division")
+print("6. Floor Division")
+print("7. Factorail")
+def again():
+	num=int(input("Enter your choice:"))
+	print("")
+	if type(num)==int:
+		if num==1:
+			print("you want to perform addition")
+			num1=int(input("enter the number:"))
+			num2=int(input("enter the second one:"))
+			add=num1+num2
+			print("addition of two number is ",add)
+			print("")
+			again1()
+		
+		elif num==2:
+			print("you want to perform subtraction")
+			num1=int(input("enter the number:"))
+			num2=int(input("enter the second one:"))
+			sub=num1-num2
+			print("subtraction of two numbers is ",sub)
+			print("")
+			again1()
+		
+		elif num==3:
+			print("you want to perform multiplication")
+			num1=int(input("enter the number:"))
+			num2=int(input("enter the second one:"))
+			mul=num1*num2
+			print("multiplication of two number is ",mul)
+			print("")
+			again1()
+				
+		elif num==4:
+			print("you want to find power")
+			num1=int(input("enter the base value:"))
+			num2=int(input("enter the index value:"))
+			pow=num1**num2
+			print("power: ",pow)
+			print("")
+			again1()
+		
+		elif num==5:
+			print("you want to perform division")
+			num1=int(input("enter the number:"))
+			num2=int(input("enter the second one:"))
+			div=num1/num2
+			print("Division of number is",div)
+			print("")
+			again1()
+			
+		elif num==6:
+			print("you want to floor value")
+			num1=int(input("enter the number:"))
+			num2=int(input("enter the second one:"))
+			flrdiv=num1//num2
+			print("result of floor division is ",flrdiv)
+			print("")
+			again1()
+			
+		elif num==7:
+			print("you want to find factorail:")
+			num=int(input("enter any number"))
+			fact=1
+			for x in range(1,num+1):
+				fact=fact*x
+			print("factorial of that number is ",fact)
+			print("")
+			again1()
+			
+		else:
+			print("enter valid number")
+			print("")
+			again()
+	else:
+		print("enter valid number")
+		print("")
+		again()
+		
+def again1():
+	print("do you want to perform again")
+	choice=input("enter 'y' to continue:")
+	if choice=="y":
+		again()
+		
+again()

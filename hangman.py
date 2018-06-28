2a7e31af2e6aec6e3c10403c339ff4617abb2bfd
malay190 <malaykumar089@gmail.com>
2018-06-28 13:32:07 +0530

    hangmanversion1.01

--- hangman.py
+++ hangman.py
@@ -4,6 +4,8 @@ import random
 import string
 import time
 
+
+#output formatting!
 print("")
 print("")
 print("			********************Welcome*********************")
@@ -33,13 +35,16 @@ print("|   / \ ")
 time.sleep(1)
 print("Let's Start")
 print("")
+
+#making lists for word slection!
 animal=["panda","tiger","lion","giraffe","cat","hippo","pyhton","leopard","chameleon","camel","Chimpanzee","buffalo","butterfly"]
 cricplayer=["virat","kohli","dhoni","pollard","rahul","sachin","yuvraj","sahwag","hardik","rohit","gayle","warner"]
 color=["red","pink","white","yellow","black","green","voilet","maroon","purple","navy","teal","beige","orange","coral"]
 car=["Nissan","Toyota"," Honda","Chevrolet","Mercedes","Volvo","Cadillac","Lexus","audi"]
 
+#hints!
 print("1.Animal")
-time.sleep(1)
+time.sleep(1)	#sleep for 1 sec!
 print("2.cricplayer")
 time.sleep(1)
 print("3.color")
@@ -48,16 +53,16 @@ print("4.car")
 time.sleep(1)
 print("5.Exit")
 time.sleep(1)
-choice=int(input("Enter your choice:"))
+choice=int(input("Enter your choice:"))	#take input from user
 
+# function define
 def animal_f():
 	lista=[]
-	animal_v=random.choice(animal).lower()
-	n=len(animal_v)
+	animal_v=random.choice(animal).lower()	#select random word from list animal
+	n=len(animal_v)	#find length of word
 	
-	for x in animal_v:
+	for x in animal_v:	#selected word is append in new list
 		lista.append(x)
-	#print(" ".join(lista))
 	
 	list=[]
 	for x in range(n):
@@ -67,90 +72,10 @@ def animal_f():
 	count=0
 	sucess=0
 	while count<9 and sucess<n:		
-		search=input("enter your guess...")
+		search=input("enter your guess...")	#take a letter as input from user
 		search=search.lower()
 		
-		if search in lista:
-			index=lista.index(search)
-			lista[index]="*"
-			list[index]=search
-			print(" ".join(list))
-			print("")
-			sucess+=1			
-		else:
-			print(" ".join(list))
-			count+=1
-			print((9-count),"chances remainig")
-			print(" ")
-				
-	if count<9 and sucess==n:
-		print(" ")
-		print("Congrats ,you won!")
-	else:
-		print("")
-		print("you loose..try again later")
-		
-def cricplayer_f():
-	lista=[]
-	cric_v=random.choice(cricplayer).lower()
-	n=len(cric_v)
-	
-	for x in cric_v:
-		lista.append(x)
-	#print(" ".join(lista))
-	
-	list=[]
-	for x in range(n):
-		list.append("_")
-	print(" ".join(list))
-	
-	count=0
-	sucess=0
-	while count<9 and sucess<n:		
-		search=input("enter your guess...")
-		search=search.lower()
-		
-		if search in lista:
-			index=lista.index(search)
-			lista[index]="*"
-			list[index]=search
-			print(" ".join(list))
-			print("")
-			sucess+=1			
-		else:
-			print(" ".join(list))
-			count+=1
-			print((9-count),"chances remainig")
-			print(" ")
-				
-	if count<9 and sucess==n:
-		print(" ")
-		print("Congrats ,you won!")
-	else:
-		print("")
-		print("you loose..try again later")
-		
-def color_f():
-	lista=[]
-	color_v=random.choice(color).lower()
-	n=len(color_v)
-	
-	for x in color_v:
-		lista.append(x)
-	#print(" ".join(lista))
-	
-	list=[]
-	for x in range(n):
-		list.append("_")
-	print(" ".join(list))
-	
-	count=0
-	sucess=0
-	while count<9 and sucess<n:		
-		search=input("enter your guess...")
-		search=search.lower()
-		
-		if search in lista:
+		if search in lista:	#if letter found in word
 			index=lista.index(search)
 			lista[index]="*"
 			list[index]=search
@@ -170,16 +95,14 @@ def color_f():
 		print("")
 		print("you loose..try again later")
 
-		
-		
-def car_f():
+#function define!		
+def cricplayer_f():
 	lista=[]
-	car_v=random.choice(car).lower()
-	n=len(car_v)
+	cric_v=random.choice(cricplayer).lower()	#select random word from list cricplayer
+	n=len(cric_v)
 	
-	for x in car_v:
+	for x in cric_v:	#selected word is append in new list
 		lista.append(x)
-	#print(" ".join(lista))
 	
 	list=[]
 	for x in range(n):
@@ -189,10 +112,90 @@ def car_f():
 	count=0
 	sucess=0
 	while count<9 and sucess<n:		
-		search=input("enter your guess...")
+		search=input("enter your guess...")	#take a letter as input from user
 		search=search.lower()
 		
-		if search in lista:
+		if search in lista:	#chech entered letter is found in word or not
+			index=lista.index(search)
+			lista[index]="*"	
+			list[index]=search
+			print(" ".join(list))
+			print("")
+			sucess+=1			
+		else:
+			print(" ".join(list))
+			count+=1
+			print((9-count),"chances remainig")
+			print(" ")
+				
+	if count<9 and sucess==n:
+		print(" ")
+		print("Congrats ,you won!")
+	else:
+		print("")
+		print("you loose..try again later")
+
+#function define!		
+def color_f():
+	lista=[]
+	color_v=random.choice(color).lower()	#select random word from list color
+	n=len(color_v)
+	
+	for x in color_v:	#selected word is append in new list
+		lista.append(x)
+	
+	list=[]
+	for x in range(n):
+		list.append("_")
+	print(" ".join(list))
+	
+	count=0
+	sucess=0
+	while count<9 and sucess<n:		
+		search=input("enter your guess...")	#take a letter as input from user
+		search=search.lower()
+		
+		if search in lista:	#chech entered letter is found in word or not
+			index=lista.index(search)
+			lista[index]="*"
+			list[index]=search
+			print(" ".join(list))
+			print("")
+			sucess+=1			
+		else:
+			print(" ".join(list))
+			count+=1
+			print((9-count),"chances remainig")
+			print(" ")
+				
+	if count<9 and sucess==n:
+		print(" ")
+		print("Congrats ,you won!")
+	else:
+		print("")
+		print("you loose..try again later")
+
+#function define!		
+def car_f():
+	lista=[]
+	car_v=random.choice(car).lower()	#select random word from list car
+	n=len(car_v)
+	
+	for x in car_v:	#selected word is append in new list
+		lista.append(x)
+	
+	list=[]
+	for x in range(n):
+		list.append("_")
+	print(" ".join(list))
+	
+	count=0
+	sucess=0
+	while count<9 and sucess<n:		
+		search=input("enter your guess...")	#take a letter as input from user
+		search=search.lower()
+		
+		if search in lista:	#chech entered letter is found in word or not
 			index=lista.index(search)
 			lista[index]="*"
 			list[index]=search
@@ -215,7 +218,8 @@ def car_f():
 def exit_f():
 	exit()
 		
-if choice==1:
+#corresponding function according to choice
+if choice==1:	
 	animal_f()
 elif choice==2:
 	cricplayer_f()
@@ -223,7 +227,7 @@ elif choice==3:
 	color_f()
 elif choice==4:
 	car_f()
-else: 
+elif choice==5: 
 	exit_f()
 	
 
